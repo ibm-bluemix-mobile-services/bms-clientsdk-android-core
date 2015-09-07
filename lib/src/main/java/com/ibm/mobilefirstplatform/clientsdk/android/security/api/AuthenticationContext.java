f5951097790077e64ea6178d1d1f4d17c47a9160
vitalym <vitalym@il.ibm.com>
2015-09-07 16:26:05 +0300

    Updated documentation.

--- lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/security/api/AuthenticationContext.java
+++ lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/security/api/AuthenticationContext.java
@@ -13,6 +13,8 @@
 
 package com.ibm.mobilefirstplatform.clientsdk.android.security.api;
 
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Response;
+
 import org.json.JSONObject;
 
 /**
@@ -38,8 +40,12 @@ public interface AuthenticationContext {
 	void submitAuthenticationSuccess ();
 
 	/**
-	 * Informs about authentication failure.
-	 * @param info Extended information about the failure.
+	 * Informs about authentication failure. This function must be called from a custom challenge
+	 * handler when the authorization request should be canceled for any reason (for example,
+	 * when user clicks 'cancel' on login dialog). The original {@link com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResourceRequest}
+	 * will be failed.
+	 * @param info Extended information about the failure. It will be passed to {@link com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResponseListener#onFailure(Response, Throwable, JSONObject)} of
+	 *             the resource request as 'extendedInfo' object.
 	 */
 	void submitAuthenticationFailure (JSONObject info);
 
