2c2dcd027e4a52aa83fb79ec445369f7940ac4f3
vitalym <vitalym@il.ibm.com>
2015-09-16 12:48:25 +0300

    Refactor BMSClient. Now it extends MFPClient. Remove FailResponse / FailResponseTest.

--- lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/core/api/BMSClient.java
+++ lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/core/api/BMSClient.java
@@ -16,32 +16,24 @@ package com.ibm.mobilefirstplatform.clientsdk.android.core.api;
 import android.content.Context;
 
 import com.ibm.mobilefirstplatform.clientsdk.android.logger.api.Logger;
-import com.ibm.mobilefirstplatform.clientsdk.android.security.api.AuthenticationListener;
 import com.ibm.mobilefirstplatform.clientsdk.android.security.api.AuthorizationManager;
 import com.ibm.mobilefirstplatform.clientsdk.android.security.internal.Utils;
-import com.ibm.mobilefirstplatform.clientsdk.android.security.internal.challengehandlers.ChallengeHandler;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.security.InvalidParameterException;
-import java.util.HashMap;
 
 /**
  * The BMSClient is a singleton that serves as the entry point to MobileFirst.
  */
-public class BMSClient {
+public class BMSClient extends MFPClient {
     public final static String HTTP_SCHEME = "http";
     public final static String HTTPS_SCHEME = "https";
 
     private final static String QUERY_PARAM_SUBZONE = "subzone";
 
-    private static BMSClient instance = null;
     private String backendRoute;
     private String backendGUID;
     private String rewriteDomain;
-
-    private int defaultTimeout = 20000;
-    private HashMap<String, ChallengeHandler> challengeHandlers = new HashMap<>();
 
     /**
      * Should be called to obtain the instance of BMSClient.
@@ -53,7 +45,8 @@ public class BMSClient {
 
             MFPRequest.setup(); //Set up network interceptor to log network event times analytics.
         }
-        return instance;
+
+        return (BMSClient)instance;
     }
 
     private BMSClient() {
@@ -65,13 +58,13 @@ public class BMSClient {
      * This method should be called before you send the first request that requires authorization.
      * </p>
      * @param context Android application context
-     * @param backendRoute Specifies the base URL for the authorization server
-     * @param backendGUID Specifies the GUID of the application
+     * @param bluemixAppRoute Specifies the base URL for the authorization server
+     * @param bluemixAppGUID Specifies the GUID of the application
      * @throws MalformedURLException {@code backendRoute} could not be parsed as a URL.
      */
-    public void initialize(Context context, String backendRoute, String backendGUID) throws MalformedURLException {
-        this.backendGUID = backendGUID;
-        this.backendRoute = backendRoute;
+    public void initialize(Context context, String bluemixAppRoute, String bluemixAppGUID) throws MalformedURLException {
+        this.backendGUID = bluemixAppGUID;
+        this.backendRoute = bluemixAppRoute;
         this.rewriteDomain = null;
         String subzone = null;
 
@@ -94,7 +87,7 @@ public class BMSClient {
      *
      * @return backend route url
      */
-    public String getBackendRoute() {
+    public String getBluemixAppRoute() {
         return backendRoute;
     }
 
@@ -102,63 +95,8 @@ public class BMSClient {
      *
      * @return backend GUID
      */
-    public String getBackendGUID() {
+    public String getBluemixAppGUID() {
         return backendGUID;
-    }
-
-    /**
-     * Registers authentication listener for specified realm.
-     * @param realm authentication realm.
-     * @param authenticationListener authentication listener.
-     */
-    public void registerAuthenticationListener(String realm, AuthenticationListener authenticationListener) {
-        if (realm == null || realm.isEmpty()) {
-            throw new InvalidParameterException("The realm name can't be null or empty.");
-        }
-
-        if (authenticationListener == null) {
-            throw new InvalidParameterException("The authentication listener object can't be null.");
-        }
-
-        ChallengeHandler handler = new ChallengeHandler();
-        handler.initialize(realm, authenticationListener);
-        challengeHandlers.put(realm, handler);
-    }
-
-    /**
-     * Unregisters authentication listener
-     * @param realm the realm the listener was registered for
-     */
-    public void unregisterAuthenticationListener(String realm) {
-        if (realm != null && !realm.isEmpty()) {
-            challengeHandlers.remove(realm);
-        }
-    }
-
-    /**
-     * @exclude
-     *
-     * @param realm authentication realm
-     * @return challenge handler for specified realm
-     */
-    public ChallengeHandler getChallengeHandler(String realm) {
-        return challengeHandlers.get(realm);
-    }
-
-    /**
-     *
-     * @return default timeout, in milliseconds
-     */
-    public int getDefaultTimeout() {
-        return defaultTimeout;
-    }
-
-    /**
-     * Sets the default timeout. The SDK's default value is 20000 milliseconds.
-     * @param timeout specifies the new timeout, in milliseconds
-     */
-    public void setDefaultTimeout(int timeout) {
-        defaultTimeout = timeout;
     }
 
     /**
