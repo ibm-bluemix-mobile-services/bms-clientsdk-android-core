1c29cd1728f82b4325cca6a324bf178e2d63df6c
Daniel Gonzalez <dgonz@us.ibm.com>
2016-06-09 11:07:06 -0500

    Fixed incorrect TLS version specification.

--- lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/core/internal/TLSEnabledSSLSocketFactory.java
+++ lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/core/internal/TLSEnabledSSLSocketFactory.java
@@ -61,7 +61,7 @@ public class TLSEnabledSSLSocketFactory extends SSLSocketFactory {
 
     private Socket enableTLSOnSocket(Socket socket) {
         if(socket != null && (socket instanceof SSLSocket)) {
-            ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.0, TLSv1.1, TLSv1.2"});
+            ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.1", "TLSv1.2"});
         }
         return socket;
     }
